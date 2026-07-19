315c2133542ab72d6616703b7d3df57d50aece2d
Dylan Archer <mdsarcher@icloud.com>
2026-07-19 12:11:56 +1000

    Optimize CSS loading

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+
+const CSS_BUNDLE_FILES = [
+    "css/base.css",
+    "css/header-and-profile.css",
+    "css/buttons-and-dropdowns.css",
+    "css/projects.css",
+    "css/footer.css",
+    "css/utilities.css"
+];
+
+function bundleCss() {
+    const bundled = CSS_BUNDLE_FILES
+        .map((file) => fs.readFileSync(path.join(__dirname, file), "utf8"))
+        .join("\n");
+    fs.writeFileSync(path.join(__dirname, "stylesheet.css"), bundled);
+}
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("Images");
     eleventyConfig.addPassthroughCopy("Scripts");
     eleventyConfig.addPassthroughCopy("stylesheet.css");
     eleventyConfig.addPassthroughCopy("Resume.pdf");
-    eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("robots.txt");
+    eleventyConfig.addWatchTarget("css");
+    eleventyConfig.on("eleventy.before", bundleCss);
 
     eleventyConfig.addFilter("encodeUri", (value) => encodeURI(value));
 
