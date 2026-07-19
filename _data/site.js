6ab8170bb6a691bcf141674266c0b4c09c2eb9e2
Dylan Archer <mdsarcher@icloud.com>
2026-07-19 11:57:49 +1000

    Optimised images. Forgot to stage changes

--- _data/site.js
+++ _data/site.js
@@ -2,5 +2,5 @@ module.exports = {
     year: new Date().getFullYear(),
     url: "https://dylan-archer-dev.com",
     name: "Dylan Archer",
-    defaultImage: "Images/ProfilePicture.jpeg"
+    defaultImage: "Images/ProfilePicture.webp"
 };
